a10782be80afddc961834dad2db9a9fbbb7ff56d
Guido Enrique <guidoenr4@gmail.com>
2020-04-27 16:50:38 -0300

    hay que ver como modelar flama los medios de pago, el resto de diez

--- src/main/java/Egresos/Pago.java
+++ src/main/java/Egresos/Pago.java
@@ -3,3 +3,25 @@ package Egresos;
 public interface Pago {
 
 }
+
+class TarjetaCredito implements Pago{
+
+}
+
+class TarjetaDebito implements Pago{
+
+}
+
+class Efectivo implements Pago{
+
+}
+
+class DineroEnCuenta implements Pago{
+
+}
+
+class CajeroAutomatico implements Pago{
+
+}
+
+
