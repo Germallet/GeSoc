325257bdfc0c2dda23624774aff3b50a6e2db997
martirolon <martirolon.mr@gmail.com>
2020-05-07 16:24:43 -0300

    cambio la herencia en Item por una composicion mediante un TipoDeItem

---
+++ src/main/java/Egresos/TipoDeItem.java
@@ -0,0 +1,11 @@
+package Egresos;
+
+// deberiamos ver en las entregas siguientes si nos conviene que sea una interface o una clase
+interface TipoDeItem {
+}
+
+class Producto implements TipoDeItem {
+}
+
+class Servicio implements TipoDeItem {
+}
