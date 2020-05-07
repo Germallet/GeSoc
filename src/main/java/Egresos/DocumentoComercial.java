aaa84c865e542d21d561357f9800f8592f5be905
martirolon <martirolon.mr@gmail.com>
2020-05-07 16:29:38 -0300

    agrego los tipos de Doc comercial

--- src/main/java/Egresos/DocumentoComercial.java
+++ src/main/java/Egresos/DocumentoComercial.java
@@ -2,9 +2,12 @@ package Egresos;
 
 // puede ser tanto una interface como una clase, eso lo van a determinar los requerimientos de las proximas entregas
 
-public interface DocumentoComercial {
-}
-
-class Factura implements DocumentoComercial{ // el tipo ya se conoce por la class
+public class DocumentoComercial {
     int numero;
+    TipoDeDocumentoComercial tipo;
+
+    DocumentoComercial(int numero, TipoDeDocumentoComercial tipo){
+        this.numero = numero;
+        this.tipo = tipo;
+    }
 }
