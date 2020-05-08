302e38cce0d2125244d1614ba58e35ec2567e2e1
Germallet <germanmallet@gmail.com>
2020-05-07 22:54:28 -0300

    Interfaces a Enums

--- src/main/java/Seguridad/TipoDeUsuario.java
+++ src/main/java/Seguridad/TipoDeUsuario.java
@@ -1,17 +1,6 @@
 package Seguridad;
 
-// creo que es mejor una clase en vez del Enum porque probablemente los usuarios tengan comportamiento
-
-public interface TipoDeUsuario{
-}
-
-class Administrador implements TipoDeUsuario{
-}
-
-class Estandar implements TipoDeUsuario{
-}
-
-/*public enum TipoDeUsuario {
+public enum TipoDeUsuario {
     Estándar,
     Administrador
-}*/
+}
