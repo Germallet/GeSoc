c7b0fc09b26ebf0407c88f389ee3b85010e57770
ceciliaarocca <ceci_boro@hotmail.com>
2020-06-15 17:00:23 -0300

    Agrego idea comprador

---
+++ src/main/java/Egresos/Comprador.java
@@ -0,0 +1,12 @@
+package Egresos;
+
+import Proveedor.Proveedor;
+
+import java.util.List;
+
+public class Comprador {
+
+    List<Proveedor> proveedores;
+
+    //planteo esta idea para elegir un presupuesto segun criterio
+}
