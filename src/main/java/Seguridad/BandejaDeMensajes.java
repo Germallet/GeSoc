80c7f6cecb29e054326ab4db1b82992e2f1367ce
unknown <ceci_boro@hotmail.com>
2020-09-22 22:57:21 -0300

    .

--- src/main/java/Seguridad/BandejaDeMensajes.java
+++ src/main/java/Seguridad/BandejaDeMensajes.java
@@ -3,6 +3,7 @@ package Seguridad;
 import Main.IDGenerator;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @Entity
 public class BandejaDeMensajes extends IDGenerator {
     @OneToMany
+    @JoinColumn(name="id_mensaje",referencedColumnName = "id")
     private List<Mensaje> mensajes = new ArrayList<>();
 
     public void recibirMensaje(Mensaje nuevoMensaje) {
