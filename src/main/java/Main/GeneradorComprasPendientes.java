7fb68fbbdd702d988845e51c564d44bc47f612a9
Germallet <germanmallet@gmail.com>
2020-10-13 20:28:54 -0300

    Renombrado Main a GeneradorComprasPendientes

--- src/main/java/Main/GeneradorComprasPendientes.java
+++ src/main/java/Main/GeneradorComprasPendientes.java
@@ -1,6 +1,6 @@
 package Main;
 
-public class Main {
+public class GeneradorComprasPendientes {
     public static void main(String[] args) {
         System.out.println("Generando compras pendientes");
         RepoOrganizaciones.repositorio().obtenerOrganizaciones().forEach(organizacion -> organizacion.validarEgresos());
