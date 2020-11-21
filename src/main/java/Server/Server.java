f7fc3a1e96963bbb1356f15b979e02e938fc0845
Germallet <germanmallet@gmail.com>
2020-11-21 14:06:37 -0300

    Puerto Heroku

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -5,8 +5,15 @@ import spark.debug.DebugScreen;
 
 public class Server {
     public static void main(String[] args) {
-        Spark.port(80);
+        Spark.port(puerto());
         DebugScreen.enableDebugScreen();
         Router.configure();
+    }
+
+    private static int puerto() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null)
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        return 80;
     }
 }
